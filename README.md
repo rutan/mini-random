ae68f6453a4d21a061a2f37def8e9b544af357f8
ru_shalm <ru_shalm@hazimu.com>
2017-05-24 01:40:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# MiniRandom
+
+[![npm version](https://badge.fury.io/js/%40rutan%2Fmini-random.svg)](https://badge.fury.io/js/%40rutan%2Fmini-random)
+
+## install
+
+```
+npm install @rutan/mini-random
+```
+
+## Usage
+
+```javascript
+import MiniRandom from '@rutan/mini-random';
+
+const random = MiniRandom.create();
+random.rand(); // => 0.8618663500207165
+random.randInt(100); // => 10
+```
+
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+export default class MiniRandom {
+    public static create() {
+        const generator = new MiniRandom(Math.floor(Math.random() * 123456789));
+        return generator;
+    }
+
+    private _x: number;
+    private _y: number;
+    private _z: number;
+    private _w: number;
+
+    constructor(x = 123456789, y = 362436069, z = 521288629, w = 88675123) {
+        this.loadSeed(x, y, z, w);
+    }
+
+    public loadSeed(x: number, y: number, z: number, w: number) {
+        this._x = x;
+        this._y = y;
+        this._z = z;
+        this._w = w;
+    }
+
+    public dumpSeed() {
+        return [this._x, this._y, this._z, this._w];
+    }
+
+    public rand() {
+        return this._step() / 4294967295;
+    }
+
+    public randInt(max: number) {
+        return Math.floor(this.rand() * max);
+    }
+
+    private _step() {
+        const t = this._x ^ (this._x << 11);
+        this._x = this._y;
+        this._y = this._z;
+        this._z = this._w;
+        this._w = (this._w ^ (this._w >>> 19)) ^ (t ^ (t >>> 8));
+        return this._w >>> 0;
+    }
+}
