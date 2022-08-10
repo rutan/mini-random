f35125e9f03429db9a84ecc92acc1ae3472b08d4
rutan <ru_shalm@hazimu.com>
2022-08-11 01:20:33 +0900

    upgrade modules

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,29 +1,28 @@
-import * as assert from 'power-assert';
 import MiniRandom from '../src/index';
 
 describe('MiniRandom', () => {
   describe('create', () => {
     test('callable create', () => {
       const miniRandom = MiniRandom.create();
-      assert.ok(miniRandom);
+      expect(miniRandom).toBeTruthy();
     });
   });
 
   describe('rand', () => {
     test('valid value', () => {
       const miniRandom = new MiniRandom();
-      assert.ok(miniRandom.rand() === 3701687786 / 4294967295);
-      assert.ok(miniRandom.rand() === 458299110 / 4294967295);
-      assert.ok(miniRandom.rand() === 2500872618 / 4294967295);
+      expect(miniRandom.rand()).toBe(3701687786 / 4294967295);
+      expect(miniRandom.rand()).toBe(458299110 / 4294967295);
+      expect(miniRandom.rand()).toBe(2500872618 / 4294967295);
     });
   });
 
   describe('randInt', () => {
     test('valid value', () => {
       const miniRandom = new MiniRandom();
-      assert.ok(miniRandom.randInt(4294967295) === 3701687786);
-      assert.ok(miniRandom.randInt(4294967295) === 458299110);
-      assert.ok(miniRandom.randInt(4294967295) === 2500872618);
+      expect(miniRandom.randInt(4294967295)).toBe(3701687786);
+      expect(miniRandom.randInt(4294967295)).toBe(458299110);
+      expect(miniRandom.randInt(4294967295)).toBe(2500872618);
     });
   });
 
@@ -31,10 +30,10 @@ describe('MiniRandom', () => {
     test('use seed', () => {
       const miniRandom1 = MiniRandom.create();
       const miniRandom2 = new MiniRandom();
-      assert.ok(miniRandom1.rand() !== miniRandom2.rand());
+      expect(miniRandom1.rand()).not.toBe(miniRandom2.rand());
       const seeds = miniRandom1.dumpSeed();
       miniRandom2.loadSeed(seeds[0], seeds[1], seeds[2], seeds[3]);
-      assert.ok(miniRandom1.rand() === miniRandom2.rand());
+      expect(miniRandom1.rand()).toBe(miniRandom2.rand());
     });
   });
 });
